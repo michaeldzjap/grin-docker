e404e068aa3c67fb978fc35c3dc3b19442407b19
Michael Dzjaparidze <michael.dzjaparidze@gmail.com>
2019-05-03 20:51:30 +0200

    Add supervisor so we can run owner API listener and regular wallet listener simultaneously

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,34 +1,24 @@
 #!/usr/bin/env bash
 set -ex
 
-if [ $GRIN_NET = 'floonet' ]; then
-    net='--floonet'
-    dir='floo'
-else
-    net=''
-    dir='main'
-fi
-
-path=/root/.grin/$dir
-
-if [ ! -f $path/grin-server.toml ]; then
-    if [ ! -d $path ]; then
+if [ ! -f $GRIN_PATH/grin-server.toml ]; then
+    if [ ! -d $GRIN_PATH ]; then
         # Create the given directory if it doesn't exist yet
-        mkdir $path
+        mkdir $GRIN_PATH
     fi
 
     # Publish the Grin server configuration file
-    (cd $path; grin $net server config)
+    (cd $GRIN_PATH; grin $GRIN_NET_ARG server config)
 fi
 
 # Don't run the ncurses TUI
-sed -i -e 's/run_tui = true/run_tui = false/' $path/grin-server.toml
+sed -i -e 's/run_tui = true/run_tui = false/' $GRIN_PATH/grin-server.toml
 
 # Ensure the API is reachable from a remote connection
-sed -i -e 's/api_http_addr = "127.0.0.1:13413"/api_http_addr = "0.0.0.0:13413"/' $path/grin-server.toml
+sed -i -e 's/api_http_addr = "127.0.0.1:13413"/api_http_addr = "0.0.0.0:13413"/' $GRIN_PATH/grin-server.toml
 
 # Overwrite the API key
-echo $GRIN_API_SECRET > $path/.api_secret
+echo $GRIN_API_SECRET > $GRIN_PATH/.api_secret
 
 # Run a Grin node
-grin $net
+grin $GRIN_NET_ARG
--- wallet/entrypoint.sh
+++ wallet/entrypoint.sh
@@ -1,39 +1,29 @@
 #!/usr/bin/env bash
 set -ex
 
-if [ $GRIN_NET = 'floonet' ]; then
-    net='--floonet'
-    dir='floo'
-else
-    net=''
-    dir='main'
-fi
-
-path=/root/.grin/$dir
-wallet_path=$path/wallet_data
-
-if [ ! -f $path/grin-wallet.toml ]; then
+if [ ! -f $GRIN_PATH/grin-wallet.toml ]; then
     # Create the wallet
-    expect -c 'spawn grin-wallet '$net' init; expect "Password:"; send '$GRIN_WALLET_PASSWORD'\r; expect "Confirm Password:"; send '$GRIN_WALLET_PASSWORD'\r; expect eof'
+    expect -c 'spawn grin-wallet '$GRIN_NET_ARG' init; expect "Password:"; send '$GRIN_WALLET_PASSWORD'\r; expect "Confirm Password:"; send '$GRIN_WALLET_PASSWORD'\r; expect eof'
 fi
 
 if [ $GRIN_INCLUDE_OWNER_API = true ]; then
     # Include the foreign API endpoints on the same port
-    sed -i -e 's/owner_api_include_foreign = false/owner_api_include_foreign = true/' $path/grin-wallet.toml
+    sed -i -e 's/owner_api_include_foreign = false/owner_api_include_foreign = true/' $GRIN_PATH/grin-wallet.toml
 else
     # Don't include the foreign API endpoints on the same port
-    sed -i -e 's/owner_api_include_foreign = true/owner_api_include_foreign = false/' $path/grin-wallet.toml
+    sed -i -e 's/owner_api_include_foreign = true/owner_api_include_foreign = false/' $GRIN_PATH/grin-wallet.toml
 fi
 
 # # Ensure we can receive Grin (similar to specifying the "-e" option to "grin-wallet" command)
-# sed -i -e 's/api_listen_interface = "127.0.0.1"/api_listen_interface = "0.0.0.0"/' $path/grin-wallet.toml
+# sed -i -e 's/api_listen_interface = "127.0.0.1"/api_listen_interface = "0.0.0.0"/' $GRIN_PATH/grin-wallet.toml
 
 # Overwrite the API key
-echo $GRIN_API_SECRET > $path/.api_secret
+echo $GRIN_API_SECRET > $GRIN_PATH/.api_secret
 
-if [ ! -d $path/owner ]; then
-    mkdir $path/owner
+if [ ! -d $GRIN_PATH/owner ]; then
+    # Create the folder for the owner API wallet listener
+    mkdir $GRIN_PATH/owner
 fi
 
-# Start the Grin wallet listener, including owner API
-(cd $path/owner; grin-wallet $net -e -r http://$GRIN_SERVER_HOST:$GRIN_SERVER_PORT -p $GRIN_WALLET_PASSWORD owner_api)
+# Run Supervisor (starts a wallet listener and owner API listener)
+/usr/bin/supervisord
