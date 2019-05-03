76329295b5c7a885af3718e841fe95a62677a8d0
Michael Dzjaparidze <michael.dzjaparidze@gmail.com>
2019-05-03 12:46:02 +0200

    Add example .env, add option to include / exclude owner API

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -19,13 +19,13 @@ if [ ! -f $path/grin-server.toml ]; then
 
     # Publish the Grin server configuration file
     (cd $path; grin $net server config)
-
-    # Don't run the ncurses TUI
-    sed -i -e 's/run_tui = true/run_tui = false/' $path/grin-server.toml
-
-    # Ensure the API is reachable from a remote connection
-    sed -i -e 's/api_http_addr = "127.0.0.1:13413"/api_http_addr = "0.0.0.0:13413"/' $path/grin-server.toml
 fi
+
+# Don't run the ncurses TUI
+sed -i -e 's/run_tui = true/run_tui = false/' $path/grin-server.toml
+
+# Ensure the API is reachable from a remote connection
+sed -i -e 's/api_http_addr = "127.0.0.1:13413"/api_http_addr = "0.0.0.0:13413"/' $path/grin-server.toml
 
 # Overwrite the API key
 echo $GRIN_API_SECRET > $path/.api_secret
--- wallet/entrypoint.sh
+++ wallet/entrypoint.sh
@@ -15,13 +15,18 @@ wallet_path=$path/wallet_data
 if [ ! -f $path/grin-wallet.toml ]; then
     # Create the wallet
     expect -c 'spawn grin-wallet '$net' init; expect "Password:"; send '$GRIN_WALLET_PASSWORD'\r; expect "Confirm Password:"; send '$GRIN_WALLET_PASSWORD'\r; expect eof'
+fi
 
+if [ $GRIN_INCLUDE_OWNER_API = true ]; then
     # Include the foreign API endpoints on the same port
     sed -i -e 's/owner_api_include_foreign = false/owner_api_include_foreign = true/' $path/grin-wallet.toml
-
-    # # Ensure we can receive Grin (similar to specifying the "-e" option to "grin-wallet" command)
-    # sed -i -e 's/api_listen_interface = "127.0.0.1"/api_listen_interface = "0.0.0.0"/' $path/grin-wallet.toml
+else
+    # Don't include the foreign API endpoints on the same port
+    sed -i -e 's/owner_api_include_foreign = true/owner_api_include_foreign = false/' $path/grin-wallet.toml
 fi
+
+# # Ensure we can receive Grin (similar to specifying the "-e" option to "grin-wallet" command)
+# sed -i -e 's/api_listen_interface = "127.0.0.1"/api_listen_interface = "0.0.0.0"/' $path/grin-wallet.toml
 
 # Overwrite the API key
 echo $GRIN_API_SECRET > $path/.api_secret
@@ -30,5 +35,5 @@ if [ ! -d $path/owner ]; then
     mkdir $path/owner
 fi
 
-# Start the Grin wallet listener
+# Start the Grin wallet listener, including owner API
 (cd $path/owner; grin-wallet $net -e -r http://$GRIN_SERVER_HOST:$GRIN_SERVER_PORT -p $GRIN_WALLET_PASSWORD owner_api)
